4573ec813d7dfd3b39d76dd24323161549333538
alvaro <alvaro_dcp@outlook.com>
2022-06-29 16:50:05 -0300

    Added cors

--- src/shared/infra/http/server.ts
+++ src/shared/infra/http/server.ts
@@ -1,6 +1,7 @@
 import 'reflect-metadata';
 
 import 'express-async-errors';
+import cors from 'cors';
 import express, { NextFunction, Request, Response } from 'express';
 import routes from './routes';
 import uploadConfig from '@config/upload';
@@ -9,6 +10,7 @@ import '@shared/container';
 
 const app = express();
 
+app.use(cors());
 app.use(express.json());
 app.use('/files', express.static(uploadConfig.uploadsFolder));
 app.use(routes);
